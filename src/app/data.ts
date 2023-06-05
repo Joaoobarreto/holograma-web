a768d32cc78ecfc2dba74399fd5846f071d2e265
João Cardoso <gabrielbcardoso@hotmail.com>
2023-06-05 11:03:43 -0300

    tela inicial

--- src/app/data.ts
+++ src/app/data.ts
@@ -66,5 +66,65 @@ export const data = {
       img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
       price: 89.90
     },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
+    {
+      id: 1,
+      title: 'Darth Vader',
+      img: 'C:/Users/Joao/Desktop/holograma/assets/img/holo-teste.jpg',
+      price: 89.90
+    },
   ]
 }
